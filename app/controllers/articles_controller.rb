62722303a0f06189b895a6aae050a57e1a75d472
Hongbin Zhou <hongbinzhou@hotmail.com>
2016-02-14 23:31:04 +0800

    add article new action and render create page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,8 @@
 class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+    render plain: params[:article].inspect # :article is a symbol, is the key of the hash
+  end
 end
